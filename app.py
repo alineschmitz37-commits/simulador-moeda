5129d3b31a14af5ebf1c673ee8acd708a3055c9b
alineschmitz37-commits <alineschmitz37@gmail.com>
2026-08-24 16:34:41 -0700

    Implement coin toss simulation in Streamlit app
    Added a coin toss simulation feature using Streamlit and SciPy.

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
+import scipy.stats
 import streamlit as st
-st.headre('Jogando uma moeda')
-st.write('Ainda nao e um aplicativo funcional. Em construcao.')
+import time
+
+st.header('Jogando uma moeda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n): # função que emula o lançamento de uma moeda
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+    
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+    
+    for I in trial_outcomes:
+        outcome_no += 1
+        if I == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+        
+    return mean
+
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+start_button = st.button('Executar')
+
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+    toss_coin(number_of_trials)
